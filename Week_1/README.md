d8ae5e7a0e5e7c38b6089e3de51cd1c4063c4725
seffu <seffukioi@gmail.com>
2022-05-16 17:11:48 +0300

    intro to docker

---
+++ Week_1/README.md
@@ -0,0 +1,2 @@
+docker build -t test:pandas .
+docker run -it test:pandas
---
+++ Week_1/docker_sql/pipeline.py
@@ -0,0 +1,9 @@
+import sys
+
+import pandas as pd
+
+print(sys.argv[1])
+
+day = sys.argv[1]
+
+print(f"Successfully read {day}")
